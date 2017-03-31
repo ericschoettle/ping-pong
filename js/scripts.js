6237338f4528b21773bc1cdc0b8d2307cbe1d516
ericschoettle <eric.schoettle@gmail.com>
2017-03-31 14:19:58 -0700

    switch colors on hover works on the remove side

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,11 @@ function pingPong(number) {
 $(document).ready(function() {
   $("#add-button1, add-button2").click(function(event) {
     event.preventDefault();
-    var input = $("input#number").val();
+    var input = $("input#number1").val();
 
     var output = pingPong(input)
     $("#response").show()
-    $("input#number").val('');
+    $("input#number1").val('');
     if (output) {
       $("#response").append("<p>" + output + "</p>");
     }
@@ -32,21 +32,37 @@ $(document).ready(function() {
     debugger
     $("#add-div").hide()
     $("#remove-div").show()
-    $("input#number").val('');
-    if (output) {
-      $("#response").append("<p>" + output + "</p>");
-    }
+    $("input#number2").val('');
+    // if (output) {
+      // $("#response").append("<p>" + output + "</p>");
+    // }
   });
   $("#remove-button1").hover(function() {
     $(this).removeClass("btn-default")
     $(this).addClass("btn-primary")
     $(this).prev().removeClass("btn-primary")
     $(this).prev().addClass("btn-default")
+    $("input#number1").attr("placeholder","Enter a piece of text seen below...")
     }, function() {
     $(this).removeClass("btn-primary")
     $(this).addClass("btn-default")
     $(this).prev().removeClass("btn-default")
     $(this).prev().addClass("btn-primary")
+    $("input#number1").attr("placeholder","Enter a number...")
+    }
+  );
+  $("#add-button2").hover(function() {
+    $(this).removeClass("btn-default")
+    $(this).addClass("btn-primary")
+    $(this).next().removeClass("btn-primary")
+    $(this).next().addClass("btn-default")
+    $("input#number2").attr("placeholder","Enter a number")
+    }, function() {
+    $(this).removeClass("btn-primary")
+    $(this).addClass("btn-default")
+    $(this).next().removeClass("btn-default")
+    $(this).next().addClass("btn-primary")
+    $("input#number1").attr("placeholder","Enter a piece of text seen below...")
     }
   );
 });
